meedstrom/org-mem

commit a483da56b7d4a6a7b6f48a99e3295c8d27bdc677

Add getters org-mem-entry-keywords, org-mem-file-keywords
Related: https://github.com/meedstrom/org-node/issues/161

Now you should be able to get Denote #+identifier:.

org-mem-parser.el
@@ -122,6 +122,7 @@ brackets."
 
 ;;; Links, citations and active timestamps
 
+(defvar org-mem-parser--found-keywords nil)
 (defvar org-mem-parser--found-links nil)
 (defvar org-mem-parser--found-active-stamps nil)
 (defconst org-mem-parser--org-ts-regexp
@@ -265,7 +266,11 @@ the subheading potentially has an ID of its own."
     (goto-char beg)
     (while (re-search-forward org-mem-parser--org-ts-regexp end t)
       (push (org-mem-parser--time-string-to-int (match-string 0))
-            org-mem-parser--found-active-stamps)))
+            org-mem-parser--found-active-stamps))
+
+    ;; Start over and look for #+keywords
+    (goto-char beg)
+    (setq org-mem-parser--found-keywords (org-mem-parser--collect-keywords end)))
   (goto-char (or end (point-max))))
 
 
@@ -298,6 +303,28 @@ between buffer substrings \":PROPERTIES:\" and \":END:\"."
               alist))
       (forward-line 1))
     alist))
+
+
+;;; Keywords
+
+(defconst org-mem-parser--org-keyword-regexp "^[ 	]*#\\+\\(\\S-+?\\):[ 	]*\\(.*\\)$"
+  "Copy of `org-keyword-regexp'.")
+
+(defun org-mem-parser--merge-same-keywords (keywords)
+  "In alist KEYWORDS, de-dup the cars by string-concatting the cdrs."
+  (let (new-alist)
+    (cl-loop for (key . value) in keywords
+             do (if-let* ((cell (assoc key new-alist)))
+                    (setcdr cell (concat (cdr cell) " " value))
+                  (push (cons key value) new-alist)))
+    (nreverse new-alist)))
+
+(defun org-mem-parser--collect-keywords (end)
+  "Search for #+keywords until position END, and return an alist."
+  (org-mem-parser--merge-same-keywords
+   (cl-loop
+    while (re-search-forward org-mem-parser--org-keyword-regexp end t)
+    collect (cons (downcase (match-string 1)) (match-string 2)))))
 
 
 ;;; Main
@@ -462,6 +489,9 @@ between buffer substrings \":PROPERTIES:\" and \":END:\"."
               (forward-line))
             (setq RIGHT (point)) ;; End of the "front matter".
 
+            ;; TODO: Instead of searching manually for #+title & #+filetags,
+            ;; use the info from `org-mem-parser--found-keywords' (which is
+            ;; populated by `org-mem-parser--scan-visible-text').
             (goto-char LEFT)
             (when (re-search-forward "^#\\+FILETAGS:" RIGHT t)
               (when (not (eolp))
@@ -522,13 +552,13 @@ between buffer substrings \":PROPERTIES:\" and \":END:\"."
                         nil
                         TAGS
                         nil
-                        PSEUDO-ID)
+                        PSEUDO-ID
+                        org-mem-parser--found-keywords)
                 found-entries)
 
           ;; Prep
           (unless CRUMBS
             (push (list 0 1 1 nil nil nil nil) CRUMBS))
-          (setq org-mem-parser--found-active-stamps nil)
           (setq LNUM (line-number-at-pos))
 
           ;; Loop over the file's headings
@@ -543,6 +573,7 @@ between buffer substrings \":PROPERTIES:\" and \":END:\"."
                    (pos-bol)
                  (point-max))))
             (setq org-mem-parser--found-active-stamps nil)
+            (setq org-mem-parser--found-keywords nil)
             (setq STATS-COOKIES nil)
             (setq INITIAL-STATS-COOKIES nil)
             (setq CLOCK-LINES nil)
@@ -734,7 +765,8 @@ between buffer substrings \":PROPERTIES:\" and \":END:\"."
                                      append (cl-sixth crumb))))
                           TAGS
                           TODO-STATE
-                          PSEUDO-ID)
+                          PSEUDO-ID
+                          org-mem-parser--found-keywords)
                   found-entries)
             (goto-char (point-max))
             ;; NOTE: Famously slow `line-number-at-pos' fast in narrow region.

org-mem.el
@@ -323,7 +323,8 @@ Note: All tables cleared often, meant for memoizations."
   (tags-inherited        nil :read-only t :type list)
   (tags-local            nil :read-only t :type list)
   (todo-state            nil :read-only t :type string-or-nil)
-  (pseudo-id             -1  :read-only t :type integer        :documentation "Generated by `org-mem-parser--mk-id'.  Not `eq'-safe, use `=', `eql' or `equal'."))
+  (pseudo-id             -1  :read-only t :type integer        :documentation "Generated by `org-mem-parser--mk-id'.  Not `eq'-safe, use `=', `eql' or `equal'.")
+  (keywords              nil :read-only t :type list))
 
 
 ;;; To find objects to operate on
@@ -986,6 +987,12 @@ by only returning non-nil if the file has been scanned by org-mem."
                file
              (truename-cache-invalidate file)
              nil))))
+
+(defun org-mem-file-keywords (file)
+  "Alist of all #+keywords in FILE."
+  (with-memoization (org-mem--table 56 file)
+    (org-mem-parser--merge-same-keywords
+     (seq-mapcat #'org-mem-entry-keywords (org-mem-entries-in-file file)))))
 
 
 ;;; Optional: Roam aliases and refs

README.org
@@ -252,6 +252,7 @@ Getters operating on an entry object
 - =org-mem-entry-file=
 - =org-mem-entry-file-truename=
 - =org-mem-entry-id=
+- =org-mem-entry-keywords=
 - =org-mem-entry-level=
 - =org-mem-entry-lnum=
 - =org-mem-entry-olpath=
@@ -297,6 +298,7 @@ Getters operating on a file name
 - =org-mem-file-coding-system=
 - =org-mem-file-id-strict=
 - =org-mem-file-id-topmost=
+- =org-mem-file-keywords=
 - =org-mem-file-known-p=
 - =org-mem-file-line-count=
 - =org-mem-file-mtime=

org-mem.texi
@@ -397,6 +397,8 @@ Getters operating on an entry object
 @item
 @samp{org-mem-entry-id}
 @item
+@samp{org-mem-entry-keywords}
+@item
 @samp{org-mem-entry-level}
 @item
 @samp{org-mem-entry-lnum}
@@ -485,6 +487,8 @@ Getters operating on a file name
 @samp{org-mem-file-id-strict}
 @item
 @samp{org-mem-file-id-topmost}
+@item
+@samp{org-mem-file-keywords}
 @item
 @samp{org-mem-file-known-p}
 @item

commit 0440fe37e414872fbfdebeddca24207b502c0862

BREAKING: Change format of org-mem-file-keywords
Now the returned alist is in the same form that you would get
from `org-collect-keywords`.

Not a severe breakage because this function is very new (introduced
2 days ago in a483da5)

org-mem-parser.el
@@ -316,25 +316,21 @@ between buffer substrings \":PROPERTIES:\" and \":END:\"."
 (defconst org-mem-parser--org-keyword-regexp "^[ 	]*#\\+\\(\\S-+?\\):[ 	]*\\(.*\\)$"
   "Copy of `org-keyword-regexp'.")
 
-;; FIXME: We should probably not always concat them, it's just for #+title,
-;;        #+subtitle and maybe some others.
-;;        Collect a list instead, and concat afterwards.
-;;        Cf. behaviors: (org-get-title) vs (org-collect-keywords '("title"))
-;;        when you have two or more #+title lines.
 (defun org-mem-parser--merge-same-keywords (keywords)
-  "In alist KEYWORDS, de-dup the cars by string-concatting the cdrs."
+  "In alist KEYWORDS, de-dup the cars by appending the cdrs."
   (let (new-alist)
     (cl-loop for (key . value) in keywords
              do (if-let* ((cell (assoc key new-alist)))
-                    (setcdr cell (concat (cdr cell) " " value))
+                    (setcdr cell (append value (cdr cell)))
                   (push (cons key value) new-alist)))
+    (cl-loop for cell in new-alist do (setcdr cell (reverse (cdr cell))))
     (nreverse new-alist)))
 
 (defun org-mem-parser--collect-keywords (end)
-  "Search for #+keywords until position END, and return an alist."
+  "Search for #+KEYWORDS: until position END, and return an alist."
   (cl-loop
    while (re-search-forward org-mem-parser--org-keyword-regexp end t)
-   collect (cons (downcase (match-string 1)) (match-string 2))))
+   collect (cons (upcase (match-string 1)) (list (match-string 2)))))
 
 
 ;;; Main

org-mem.el
@@ -330,7 +330,7 @@ Note: All tables cleared often, meant for memoizations."
   (tags-local            nil :read-only t :type list)
   (todo-state            nil :read-only t :type string-or-nil)
   (pseudo-id             -1  :read-only t :type integer        :documentation "Generated by `org-mem-parser--mk-id'.  Not `eq'-safe, use `=', `eql' or `equal'.")
-  (keywords              nil :read-only t :type list))
+  (keywords              nil :read-only t :type list           :documentation "Alist of #+keywords: found in entry only.  See `org-mem-file-keywords'."))
 
 
 ;;; To find objects to operate on
@@ -995,7 +995,7 @@ by only returning non-nil if the file has been scanned by org-mem."
              nil))))
 
 (defun org-mem-file-keywords (file)
-  "Alist of all #+keywords in FILE."
+  "Alist of all #+keywords: in FILE, as if from `org-collect-keywords'."
   (with-memoization (org-mem--table 56 file)
     (org-mem-parser--merge-same-keywords
      (seq-mapcat #'org-mem-entry-keywords (org-mem-entries-in-file file)))))